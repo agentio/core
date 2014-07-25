ec0ed32b89f11291e5723dea31a92232e5ced8d0
Tim Burks <tim@radtastical.com>
2014-07-24 17:29:35 -0700

    Apache License, Version 2.0

--- home/go/src/home/home.go
+++ home/go/src/home/home.go
@@ -1,3 +1,18 @@
+//
+//  Copyright 2014 Radtastical Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 package main
 
 import (
@@ -69,8 +84,10 @@ func getMongoSession() (mongoSession *mgo.Session) {
 	}
 	var dialInfo mgo.DialInfo
 	dialInfo.Addrs = []string{AGENT_MONGO_HOST}
-	dialInfo.Username = "root"
-	dialInfo.Password = "agent123"
+	if AGENT_MONGO_HOST != "127.0.0.1" {
+		dialInfo.Username = "root"
+		dialInfo.Password = "agent123"
+	}
 	mongoSession, err := mgo.DialWithInfo(&dialInfo)
 	if err != nil {
 		panic(err)
@@ -101,9 +118,21 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	mongoSession := getMongoSession()
 	defer mongoSession.Close()
 	session, user, err := getSessionAndUser(r, mongoSession)
+
 	if err != nil {
 		http.Redirect(w, r, "/signin", http.StatusFound)
 	} else {
+		appsCollection := mongoSession.DB("control").C("apps")
+		var apps []interface{}
+		err = appsCollection.Find(bson.M{}).All(&apps)
+		info := struct {
+			Apps    []interface{}
+			Session Session
+		}{
+			apps,
+			session,
+		}
+
 		fmt.Println(user.Username)
 		t, _ := template.ParseFiles("templates/layout.html", "templates/home.html", "templates/topbar.html")
 		title, err := template.New("title").Parse("Home")
@@ -111,7 +140,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("There was an error", err)
 		}
 		x, _ := t.AddParseTree("title", title.Tree)
-		err2 := x.ExecuteTemplate(w, "layout", session)
+		err2 := x.ExecuteTemplate(w, "layout", info)
 		if err2 != nil {
 			fmt.Println("There was an error", err2)
 		}
